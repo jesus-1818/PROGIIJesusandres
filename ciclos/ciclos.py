f2a7bc691bd5b429bab373477aa7bc151d824169
jesus-1818 <160240591+jesus-1818@users.noreply.github.com>
2024-03-18 11:11:04 -0500

    actividad 2
    segundo corte

---
+++ ciclos/ciclos.py
@@ -0,0 +1,29 @@
+elegir = 0
+while elegir <= 5:
+
+
+    print("\n1)persona")
+    print("2)vehiculo")
+    print("3)universidad")
+    print("4)notas")
+    print("5)salir")
+    
+    
+    
+    elegir = int(input("selecione una opcion: "))
+    
+    if elegir == 1:
+        print("has selecionado persona")
+    
+    if elegir == 2:
+        print("has selecionado vehiculo")
+    
+    if elegir == 3:
+        print("has selecionado universidad")
+    
+    if elegir == 4:
+        print("has selecionado notas")
+        
+    if elegir == 5:
+        print("has salido ")
+        break
